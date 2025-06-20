e17b8e83b30d0d0e54decc73afb9722539fa2872
helix <jordanbell213@yahoo.co.uk>
2025-06-20 03:13:02 +0100

    Delete public/js/config.js

--- public/js/config.js
+++
@@ -1,9 +0,0 @@
-require.config({
-  paths: {
-    vs: "https://cdn.jsdelivr.net/npm/monaco-editor@0.44.0/min/vs"
-  }
-});
-
-let monacoEditor;
-const urlParts = window.location.pathname.split("/");
-const pasteId = urlParts[1] === "p" ? urlParts[2] : null;
